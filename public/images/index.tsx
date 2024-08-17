c55eec0da531afa1aca253142366fd4d007eb0ae
Aditya Mishra <adityamishra5889@gmail.com>
2024-08-17 18:42:46 +0530

    bugfix:minor

--- public/images/index.tsx
+++ public/images/index.tsx
@@ -21,10 +21,7 @@ import ftimg from "./footerImage.png";
 import AboutSectionFeat from "./aboutsection.png";
 import HomeScreen from "./HomeScreen.png";
 import ToolboxPage from "./toolbox.png";
-import yashSite from "./yashSite.png"
 import pp from "./adminpp.png";
-import initialPortfolio from "./initialPortfolio.png";
-import Saas from "./Saas.png";
 import re from "./re.svg"
 import tail from "./tail.svg"
 import ts from "./ts.svg"
@@ -52,10 +49,7 @@ export {
   PostgresSql,
   valo,
   ftimg,
-  yashSite,
   pp,
-  initialPortfolio,
-  Saas,
   tail,
   ts,
   re,
